87df060240a15fe9898f8e836e63004acfce6df7
Mohamed Almukhtar <mohamedalmukhtar870@gmail.com>
2020-02-24 18:18:51 -0700

    change to database parameters

--- connect.php
+++ connect.php
@@ -2,8 +2,9 @@
 
     $servername = "localhost";
     /*$dsn ='mysql:host=localhost;dbname=artBlogger';//Data source Name*/
-    $username ='root';// user to connect
-    $password ='';//
+    $username ='id12705941_mohamedalmukhtar';// user to connect
+    $password ='%pP1MB$UrsN!1O^&Yh^%';//
+    $database = "id12705941_artblogger";
 
     /*
     $options =array(
@@ -11,7 +12,7 @@
     );
     */
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=artBlogger", $username, $password);
+        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         /* $query = INSRET  INTO */
         //echo "Connected Succesfully";
